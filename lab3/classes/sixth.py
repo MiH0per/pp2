cd5cd2e884b6ece4ba7b0df9b40bc103b970383b
MiH0per <maksim.kramarov.99@mail.ru>
2024-02-22 10:36:08 +0600

    3

--- lab3/classes/sixth.py
+++ lab3/classes/sixth.py
@@ -14,5 +14,5 @@ class PrimeFilter:
 numbers = [2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 prime_filter = PrimeFilter()
 prime_numbers = prime_filter.filter_primes(numbers)
-print("Prime numbers in the list:", prime_numbers)
+print(prime_numbers)
 """
